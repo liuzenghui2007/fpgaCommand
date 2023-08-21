6e19e9f8aeea1ba6588af96b66e7e07c922162d0
lzh <240164005@qq.com>
2023-08-21 14:44:41 +0800

    '提取控制字段'

--- CommandRegister.h
+++ CommandRegister.h
@@ -22,8 +22,8 @@ enum RegisterEnum : uint32_t
     WRITE_ASIC_VCM_EN_CTRL_REG_32BIT = 0x01000003,              // 写：ASIC VCM_EN 控制寄存器
     WRITE_ASIC_FC_VCOM_EN_CTRL_REG_32BIT = 0x01000004,          // 写：ASIC FC_VCOM_EN 控制寄存器
     WRITE_ASIC_CHANNEL_STATE_CFG_REG_32X32BIT = 0x01000005,     // 写：ASIC 1024个通道开关状态配置寄存器
-    WRITE_ASIC_SINGLE_CHANNEL_STATE_CFG_REG_32BIT = 0x01000006, // 写：ASIC 单个通道开关状态配置寄存器
-    WRITE_ASIC_UNBLOCK_CHANNEL_CFG_REG_32BIT = 0x01000007,      // 写：ASIC unblock通道号配置寄存器
+    WRITE_ASIC_CHANNEL_STATE_SINGLE_CFG_REG_32BIT = 0x01000006, // 写：ASIC 单个通道开关状态配置寄存器
+    WRITE_ASIC_CHANNEL_UNBLOCK_CFG_REG_32BIT = 0x01000007,      // 写：ASIC unblock通道号配置寄存器
     READ_ASIC_STATUS_REG_32BIT = 0x81000001,                    // 读：ASIC状态寄存器
 
     // ASIC cooling control registers
@@ -54,16 +54,175 @@ struct StateBitsRange {
     int high;
 };
 
-// 读：ADC采样周期状态寄存器
-struct READ_ADC_SAMPLE_PERIOD_CFG {
+// 读写：ADC采样周期状态寄存器
+struct  ADC_SAMPLE_PERIOD_CFG {
+    StateBitsRange samplePeriod;
+    ADC_SAMPLE_PERIOD_CFG() {
+        samplePeriod = {0, 31};
+    }
+};
+
+//读写：ADC每个周期内32个采样点采样通道号状态寄存器
+struct ADC_SAMPLE_CHANNEL_CFG {
     StateBitsRange ch[32];
-    READ_ADC_SAMPLE_PERIOD_CFG() {
+    ADC_SAMPLE_CHANNEL_CFG() {
         for (int i = 0; i < 32; ++i) {
             ch[i] = { (32 - (i+1) ) * 8, (32 - i ) * 8 - 1};
         }
     }
 };
 
-READ_ADC_SAMPLE_PERIOD_CFG AdcSamplePeriod;
+
+// 读写：配置ADC ACQ 时间，最小80ns，配置单位为12.5ns.
+struct ADC_ACQ_TIME_CFG {
+    StateBitsRange adcAcqTime;
+    ADC_ACQ_TIME_CFG() {
+        adcAcqTime = {0, 31};
+    }
+};
+
+// 读写：ADC使能状态寄存器
+struct ADC_ENABLE {
+    StateBitsRange adcEnable;
+    ADC_ENABLE() {
+        adcEnable = {0, 0};
+    }
+};
+
+// 写： ADC offsetcal使能
+struct ADC_OFFSETCAL_ENABLE  {
+    StateBitsRange adcOffsetcalEnable;
+    ADC_OFFSETCAL_ENABLE() {
+        adcOffsetcalEnable = {0, 0};
+    }
+};
+
+struct ASIC_CTRL {
+    StateBitsRange mask;
+    StateBitsRange ep;
+    StateBitsRange lgp;
+    StateBitsRange all;
+    StateBitsRange unblock;
+    ASIC_CTRL() {
+        mask = {16, 21}; //mask bit 对应 bit[5：0] 设置
+        ep = {5, 5};
+        lgp = {3, 4};
+        all = {1, 2};
+        unblock =  {0, 0};
+    }
+};
+
+// ASIC POWER 控制寄存器
+struct ASIC_POWER_CTRL {
+    StateBitsRange powerEnable;
+    ASIC_POWER_CTRL() {
+        powerEnable = {0, 0};
+    }
+};
+
+// ASIC VCM_EN 控制寄存器
+struct ASIC_VCM_EN {
+    StateBitsRange vcmEnable;
+    ASIC_VCM_EN() {
+        vcmEnable = {0, 0};
+    }
+};
+
+// ASIC FC_VCOM_EN 控制寄存器
+struct ASIC_FC_VCOM_EN {
+    StateBitsRange vcomEnable;
+    ASIC_FC_VCOM_EN() {
+        vcomEnable = {0, 0};
+    }
+};
+
+// ASIC 1024 个通道开关状态配置寄存器
+// 32 * 32bit, 最高bit代表最低通道开关状态
+struct ASIC_CHANNEL_STATE {
+    StateBitsRange ch[1024];
+    ASIC_CHANNEL_STATE() {
+        for (int i = 0; i < 1024; ++i) {
+            ch[i] = { 1023 - i, 1023 -i};
+        }
+    }
+};
+
+// ASIC 单个通道开关状态配置寄存器
+struct ASIC_CHANNEL_STATE_SINGLE {
+    StateBitsRange ix;
+    StateBitsRange iy;
+    StateBitsRange state;
+    ASIC_CHANNEL_STATE_SINGLE () {
+        ix = {12, 16};
+        iy = {4, 8};
+        state = {0 ,0};
+    }
+};
+
+// ASIC  unblock通道号配置寄存器
+struct ASIC_CHANNEL_UNBLOCK {
+    StateBitsRange ix;
+    StateBitsRange iy;
+    StateBitsRange unblock;
+    ASIC_CHANNEL_UNBLOCK () {
+        ix = {12, 16};
+        iy = {4, 8};
+        unblock = {0 ,0};
+    }
+};
+
+// ASIC 状态寄存器
+struct ASIC_STATUS {
+    StateBitsRange adcOverflow;
+    StateBitsRange asicRemoveDetected;
+    StateBitsRange ep;
+    StateBitsRange lgp;
+    StateBitsRange ulgp;
+    StateBitsRange aoffp;
+    StateBitsRange aonp;
+    StateBitsRange tep;
+    StateBitsRange ASIC_PWR;
+    StateBitsRange ASIC_VCM_EN;
+    StateBitsRange FC_VCOM_EN;
+    StateBitsRange ASIC_DET;
+    StateBitsRange ASIC_LOGIC_READY;
+    ASIC_STATUS() {
+        adcOverflow = {13, 13};
+        asicRemoveDetected = {12, 12};
+        ep = {11, 11};
+        lgp = {10, 10};
+        ulgp = {9, 9};
+        aoffp = {8, 8};
+        aonp = {7, 7};
+        tep = {6, 6};
+        ASIC_PWR = {5, 5};
+        ASIC_VCM_EN = {4, 4};
+        FC_VCOM_EN = {3, 3};
+        ASIC_DET = {2, 2};
+        ASIC_LOGIC_READY = {1, 1};
+    }
+
+};
+
+ADC_SAMPLE_PERIOD_CFG _AdcSamplePeriod;
+ADC_SAMPLE_CHANNEL_CFG _AdcSampleChannel;
+ADC_ENABLE _AdcEnable;
+ADC_ACQ_TIME_CFG _AdcAcqTime;
+
+ASIC_CTRL _AsicControl;
+ASIC_POWER_CTRL _AsicPower;
+ASIC_VCM_EN _AsicVcmEn;
+ASIC_FC_VCOM_EN _asicVcomEn;
+ASIC_CHANNEL_STATE _asicChannelState;
+ASIC_CHANNEL_STATE_SINGLE _asicChannelStateSingle;
+ASIC_CHANNEL_UNBLOCK _asicChannelUnblcok;
+ASIC_STATUS _asicStatus;
+
+
+
+
+
+
+
 
 #endif // COMMANDREGISTER_H
