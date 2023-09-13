0173c3eb200ffd2413b438e66fba6e1315620e11
lzh <240164005@qq.com>
2023-09-13 17:35:24 +0800

    a

--- testEigen.h
+++ testEigen.h
@@ -17,30 +17,39 @@ void testEigen() {
     const int t = 1000 / sampleRate;
 
     Eigen::MatrixXd data(numRows, numCols);
+    Eigen::MatrixXd base(1, numCols);
     data.setRandom(); // 使用随机数填充矩阵
+    base.setConstant(2.0); // 使用随机数填充基线矩阵（一维）
 
+    std::cout << "base的shape：" << base.rows() << "x" << base.cols() << std::endl;
     while (true) {
-        Eigen::VectorXd a48 = data.colwise().mean(); // 计算每行的平均值
+        // 使用replicate函数将base的行复制为与data相同的行数，并且然后使用array()函数执行逐元素减法操作。
+        // 确保在修改后的矩阵上执行array()操作，以便逐元素地进行减法操作。
+        // 原始值
+        std::cout << "原始值：" << std::endl;
+        std::cout << data << std::endl;
 
-        // 打印a48的shape
+
+        data.array() -= base.replicate(numRows, 1).array();
+
+        // 输出整个矩阵的所有值
+        std::cout << "去基线后：" << std::endl;
+        std::cout << data << std::endl;
+
+        Eigen::MatrixXd a48 = data.colwise().mean(); // 计算每行的平均值
         std::cout << "a48的shape：" << a48.rows() << "x" << a48.cols() << std::endl;
+
+
+
+        // 均值
         std::cout << "均值后：" << std::endl;
-            for (int i = 0; i < numCols; ++i) {
-            std::cout << a48(i) << " ";
-        }
-        std::cout << std::endl;
+        std::cout << a48 << std::endl;
 
-        // 对整个向量 a48 减去 5 并乘以 2
+        // 转化，对整个向量 a48 减去 5 并乘以 2
         a48 = (a48.array() - 5) * 2;
-
-        // 打印a48的转化值
-        std::cout << "a48的shape：" << a48.rows() << "x" << a48.cols() << std::endl;
-            std::cout << "转化后：" << std::endl;
-            for (int i = 0; i < numCols; ++i) {
-            std::cout << a48(i) << " ";
-        }
-        std::cout << std::endl;
-
+        std::cout << "转电流：" << std::endl;
+        std::cout << a48 << std::endl;
+        std::cout << "----------------------------" << std::endl;
         // 重新设置data的随机数
         data.setRandom();
         // 等待a毫秒
