1708ea5af13fc087e8e505f7375fae29f1276b58
lzh <240164005@qq.com>
2023-09-13 09:43:32 +0800

    a

--- testEigen.h
+++ testEigen.h
@@ -11,8 +11,8 @@ void testEigen() {
 
     constexpr uint32_t sampleRate = 1; // 平均值采样率（每秒）
 
-    constexpr uint32_t numRows = 20; // 矩阵的行数
-    constexpr uint32_t numCols = 1024; // 矩阵的列数
+    constexpr uint32_t numRows = 5; // 矩阵的行数
+    constexpr uint32_t numCols = 10; // 矩阵的列数
 
     const int t = 1000 / sampleRate;
 
@@ -25,7 +25,18 @@ void testEigen() {
         // 打印a48的shape
         std::cout << "a48的shape：" << a48.rows() << "x" << a48.cols() << std::endl;
         std::cout << "均值后：" << std::endl;
-            for (int i = 0; i < numCols / 100; ++i) {
+            for (int i = 0; i < numCols; ++i) {
+            std::cout << a48(i) << " ";
+        }
+        std::cout << std::endl;
+
+        // 对整个向量 a48 减去 5 并乘以 2
+        a48 = (a48.array() - 5) * 2;
+
+        // 打印a48的转化值
+        std::cout << "a48的shape：" << a48.rows() << "x" << a48.cols() << std::endl;
+            std::cout << "转化后：" << std::endl;
+            for (int i = 0; i < numCols; ++i) {
             std::cout << a48(i) << " ";
         }
         std::cout << std::endl;
