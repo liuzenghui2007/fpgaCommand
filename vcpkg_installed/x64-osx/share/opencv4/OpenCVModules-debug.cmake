f30fa430baf1de59800171fdf10c845eac016073
zenghui <zenghui.liu@polyseq.com>
2023-09-23 19:08:55 +0800

    nlohmann_json找不到

---
+++ vcpkg_installed/x64-osx/share/opencv4/OpenCVModules-debug.cmake
@@ -0,0 +1,149 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "opencv_core" for configuration "Debug"
+set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_core PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_core4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_core )
+list(APPEND _cmake_import_check_files_for_opencv_core "${_IMPORT_PREFIX}/debug/lib/libopencv_core4d.a" )
+
+# Import target "opencv_flann" for configuration "Debug"
+set_property(TARGET opencv_flann APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_flann PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_flann4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_flann )
+list(APPEND _cmake_import_check_files_for_opencv_flann "${_IMPORT_PREFIX}/debug/lib/libopencv_flann4d.a" )
+
+# Import target "opencv_imgproc" for configuration "Debug"
+set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_imgproc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_imgproc4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgproc )
+list(APPEND _cmake_import_check_files_for_opencv_imgproc "${_IMPORT_PREFIX}/debug/lib/libopencv_imgproc4d.a" )
+
+# Import target "opencv_ml" for configuration "Debug"
+set_property(TARGET opencv_ml APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_ml PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_ml4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_ml )
+list(APPEND _cmake_import_check_files_for_opencv_ml "${_IMPORT_PREFIX}/debug/lib/libopencv_ml4d.a" )
+
+# Import target "opencv_photo" for configuration "Debug"
+set_property(TARGET opencv_photo APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_photo PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_photo4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_photo )
+list(APPEND _cmake_import_check_files_for_opencv_photo "${_IMPORT_PREFIX}/debug/lib/libopencv_photo4d.a" )
+
+# Import target "opencv_dnn" for configuration "Debug"
+set_property(TARGET opencv_dnn APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_dnn PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_dnn4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_dnn )
+list(APPEND _cmake_import_check_files_for_opencv_dnn "${_IMPORT_PREFIX}/debug/lib/libopencv_dnn4d.a" )
+
+# Import target "opencv_features2d" for configuration "Debug"
+set_property(TARGET opencv_features2d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_features2d PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_features2d4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_features2d )
+list(APPEND _cmake_import_check_files_for_opencv_features2d "${_IMPORT_PREFIX}/debug/lib/libopencv_features2d4d.a" )
+
+# Import target "opencv_imgcodecs" for configuration "Debug"
+set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_imgcodecs PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_imgcodecs4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_imgcodecs )
+list(APPEND _cmake_import_check_files_for_opencv_imgcodecs "${_IMPORT_PREFIX}/debug/lib/libopencv_imgcodecs4d.a" )
+
+# Import target "opencv_videoio" for configuration "Debug"
+set_property(TARGET opencv_videoio APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_videoio PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_videoio4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_videoio )
+list(APPEND _cmake_import_check_files_for_opencv_videoio "${_IMPORT_PREFIX}/debug/lib/libopencv_videoio4d.a" )
+
+# Import target "opencv_calib3d" for configuration "Debug"
+set_property(TARGET opencv_calib3d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_calib3d PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_calib3d4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_calib3d )
+list(APPEND _cmake_import_check_files_for_opencv_calib3d "${_IMPORT_PREFIX}/debug/lib/libopencv_calib3d4d.a" )
+
+# Import target "opencv_highgui" for configuration "Debug"
+set_property(TARGET opencv_highgui APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_highgui PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_highgui4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_highgui )
+list(APPEND _cmake_import_check_files_for_opencv_highgui "${_IMPORT_PREFIX}/debug/lib/libopencv_highgui4d.a" )
+
+# Import target "opencv_objdetect" for configuration "Debug"
+set_property(TARGET opencv_objdetect APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_objdetect PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_objdetect4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_objdetect )
+list(APPEND _cmake_import_check_files_for_opencv_objdetect "${_IMPORT_PREFIX}/debug/lib/libopencv_objdetect4d.a" )
+
+# Import target "opencv_stitching" for configuration "Debug"
+set_property(TARGET opencv_stitching APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_stitching PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_stitching4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_stitching )
+list(APPEND _cmake_import_check_files_for_opencv_stitching "${_IMPORT_PREFIX}/debug/lib/libopencv_stitching4d.a" )
+
+# Import target "opencv_video" for configuration "Debug"
+set_property(TARGET opencv_video APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_video PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libopencv_video4d.a"
+  )
+
+list(APPEND _cmake_import_check_targets opencv_video )
+list(APPEND _cmake_import_check_files_for_opencv_video "${_IMPORT_PREFIX}/debug/lib/libopencv_video4d.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
